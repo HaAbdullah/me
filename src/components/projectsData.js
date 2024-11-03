657cf32eab0f005efa11a85af6deb472b60e5851
HaAbdullah <abdullah.hasanjee@gmail.com>
2024-11-03 16:56:56 -0500

    Added channelytics project

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,50 +1,56 @@
 const projectsData = {
-    project1: {
-      title: 'OptiMove',
-      desc: 'Detect chessboards in images, identify pieces and positions, generate FEN, and calculate the best move',
-      techStack: 'Python, OpenCV, TensorFlow, React, JavaScript, Pandas',
-      link: 'https://github.com/HaAbdullah/TurboTypist',
-      open: 'https://project1.example.com',
-    },
-    project2: {
-      title: 'GradReadyGeese',
-      desc: 'Parses a UWaterloo transcript and uses web scraping to determine courses needed to graduate',
-      techStack: 'Python, PyPDF, Beautiful Soup, HTML/CSS, JavaScript',
-      link: 'https://github.com/HaAbdullah/StarterHacks/',
-      open: 'https://project2.example.com',
-    },
-    project3: {
-      title: 'KeyWe',
-      desc: 'Chrome extension allowing you to open essential websites with simple keystrokes',
-      techStack: 'HTML/CSS, JavaScript, Chrome Storage API',
-      link: 'https://github.com/HaAbdullah/Keywe',
-      open: 'https://project1.example.com',
-    },
-    project4: {
-      title: 'KeyWe',
-      desc: 'Chrome extension allowing you to open essential websites with simple keystrokes',
-      techStack: 'HTML/CSS, JavaScript, Chrome Storage API',
-      link: 'https://github.com/HaAbdullah/Keywe',
-      open: 'https://project1.example.com',
-    },
-    project5: {
-      title: 'CurrenSea Converter',
-      desc: 'World-wide currency converter with a minimalistic aesthetic design.',
-      techStack: 'Python, PyQt5, Fixer.io-API',
-      link: 'https://github.com/HaAbdullah/CurrenSea',
-      open: 'https://project1.example.com',
-    },
-    project6: {
-      title: 'Freelance Tracker',
-      desc: 'A tracker for freelance clients and services',
-      techStack: 'Python',
-      link: 'https://github.com/HaAbdullah/FreelanceManager',
-      open: 'https://project2.example.com',
-    },
-  
+  project1: {
+    title: "OptiMove",
+    desc: "Detect chessboards in images, identify pieces and positions, generate FEN, and calculate the best move",
+    techStack: "Python, OpenCV, TensorFlow, React, JavaScript, Pandas",
+    link: "https://github.com/HaAbdullah/TurboTypist",
+    open: "https://project1.example.com",
+  },
+  project2: {
+    title: "Channelytics",
+    desc: "Analyze the data behind Youtube videos, channels, and topics through data visualizations and comparisions.",
+    techStack:
+      "Django, Pandas, Matplotlib, PostgreSQL, React.js, TypeScript, Tailwind, Material UI, Redux",
+    link: "https://github.com/HaAbdullah/Channelytics",
+    open: "https://project2.example.com",
+  },
+  project3: {
+    title: "GradReadyGeese",
+    desc: "Parses a UWaterloo transcript and uses web scraping to determine courses needed to graduate",
+    techStack: "Python, PyPDF, Beautiful Soup, HTML/CSS, JavaScript",
+    link: "https://github.com/HaAbdullah/StarterHacks/",
+    open: "https://project2.example.com",
+  },
+  project4: {
+    title: "KeyWe",
+    desc: "Chrome extension allowing you to open essential websites with simple keystrokes",
+    techStack: "HTML/CSS, JavaScript, Chrome Storage API",
+    link: "https://github.com/HaAbdullah/Keywe",
+    open: "https://project1.example.com",
+  },
+  project5: {
+    title: "KeyWe",
+    desc: "Chrome extension allowing you to open essential websites with simple keystrokes",
+    techStack: "HTML/CSS, JavaScript, Chrome Storage API",
+    link: "https://github.com/HaAbdullah/Keywe",
+    open: "https://project1.example.com",
+  },
+  project6: {
+    title: "CurrenSea Converter",
+    desc: "World-wide currency converter with a minimalistic aesthetic design.",
+    techStack: "Python, PyQt5, Fixer.io-API",
+    link: "https://github.com/HaAbdullah/CurrenSea",
+    open: "https://project1.example.com",
+  },
+  project7: {
+    title: "Freelance Tracker",
+    desc: "A tracker for freelance clients and services",
+    techStack: "Python",
+    link: "https://github.com/HaAbdullah/FreelanceManager",
+    open: "https://project2.example.com",
+  },
 
-    // Add more projects as needed
-  };
-  
-  export default projectsData;
-  
+  // Add more projects as needed
+};
+
+export default projectsData;
